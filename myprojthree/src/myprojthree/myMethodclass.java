7c3e9d4f4a87862abe548a0f8435399e0db59ba1
gul ahmed <gabaloch2@gmail.com>
2017-10-16 09:17:57 +0500

    hi

---
+++ myprojthree/src/myprojthree/myMethodclass.java
@@ -0,0 +1,20 @@
+package myprojthree;
+
+public class myMethodclass {
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+		
+
+}
